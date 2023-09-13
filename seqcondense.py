577e43175027d7ea9daa072873185a50a87bc3ae
RPINerd <RPINerd@gmail.com>
2023-09-13 16:09:30 -0400

    Sorted out grouping by tree similarity

--- seqcondense.py
+++ seqcondense.py
@@ -13,9 +13,6 @@
 import os
 import sys
 
-from Bio import AlignIO, SeqIO
-from Bio.Align import AlignInfo
-
 import utils
 
 
@@ -26,31 +23,11 @@ def main(file):
     all the sub functions to create said set of minimal consensus seqs
     """
 
-    # Parse the provided file for sequences and store in list
-    records = []
-    try:
-        for record in SeqIO.parse(file, "fasta"):
-            records.append(record)
-    except Exception as e:
-        print(f"Error on record parsing: {e}")
-        exit
-
     # Initial alignment for tree building and grouping of sequences
-    utils.init_align(file)
+    fam_groups = utils.init_align(file)
 
-    # for sequence in records:
-    #     reference_list = records.remove(sequence)
-    #     for ref_sequence in reference_list:
-    #         alignment = AlignIO.align()
-    #         summary_align = AlignInfo.SummaryInfo(alignment)
-    #         summary_align.dumb_consensus(float(sys.argv[2]))
-
-    # if 'align better':
-    #     pass
-    #     # save to best alignment
-    # else next fasta
-
-    # save best match to new struct
+    # Associate each child in the families with their respective sequence
+    family_dict = utils.associate_seqs(fam_groups, file)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -2,12 +2,14 @@
     Utility functions for the SeqCondense pipeline
 """
 
+import itertools
 import logging
+import shlex
 import subprocess
 
-from Bio import AlignIO
+from Bio import AlignIO, SeqIO
 from Bio.Align.Applications import ClustalOmegaCommandline as clustO_cline
-from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
 
 SEED_COUNT = 1
 TMP_DIR = "./tmp/"
@@ -36,22 +38,95 @@ def align(seq_file) -> str:
     """A simple wrapper for performing a default Clustal Omega"""
 
     outfile_name = f"{TMP_DIR}{SEED_COUNT}.aln"
-    cmd = clustO_cline(infile=seq_file, outfile=outfile_name, outfmt="phy", threads=8)
-    logging.debug(f"clustal call: {cmd}")
-    subprocess.check_call(cmd)
+    cmd = clustO_cline(infile=seq_file, outfile=outfile_name, outfmt="phy", threads=8, force=True)
+    call = shlex.split(str(cmd))
+    logging.debug(f"clustal call: {call}")
+    subprocess.check_call(call)
     return outfile_name
 
 
-def init_align(ref_file) -> None:
+def generate_family_groups(tree) -> list:
+    """
+    Get the non-terminal clades, and for each one of them, check if any
+    of the children are terminals. If so, add them to a family. The end
+    result are a list of clades which are grouped in most-similar families.
+    """
+
+    nonTerminalClades = tree.get_nonterminals()
+    groups = []
+    for clade in nonTerminalClades:
+        # If any of the children are terminals, add them to a family
+        if is_semipreterminal(clade):
+            family = []
+            for child in clade:
+                if child.is_terminal():
+                    family.append(child)
+            # Once all terminal children are gathered, add the family to the list
+            # of groups if the family list is not empty
+            groups.append(family) if family else None
+
+    for fam in groups:
+        logging.debug(f"Family: {[x.name for x in fam]}")
+
+    return groups
+
+
+def associate_seqs(fam_groups, file):
+    """
+    Associate each child in the families with their respective sequence
+    """
+
+    # Parse the provided file for sequences and store in a dictionary
+    records = {}
+    try:
+        for record in SeqIO.parse(file, "fasta"):
+            records[record.ID] = record.seq
+    except Exception as e:
+        print(f"Error on record parsing: {e}")
+        exit
+
+    # Associate each child in the families with their respective sequence
+    associated_fam_groups = []
+    for fam in fam_groups:
+        new_fam = {}
+        for child in fam:
+            child.seq = next(record.seq for record in records if record.id == child.name)
+
+
+def terminal_neighbor_dists(self):
+    """Return a list of distances between adjacent terminals."""
+
+    def generate_pairs(self):
+        pairs = itertools.tee(self)
+        next(pairs[1])
+        return zip(pairs[0], pairs[1])
+
+    return [self.distance(*i) for i in generate_pairs(self.find_clades(terminal=True))]
+
+
+def is_semipreterminal(clade):
+    return any(child.is_terminal() for child in clade)
+
+
+def init_align(ref_file):
     """
     Calls align to generate an MSA with all initial sequences then generates
     a pylogenetic tree to group sequences into most similar
     """
 
+    # Create alignment and parse it into a AlignIO object
     alignment = align(ref_file)
     bioalign_obj = AlignIO.read(alignment, "phylip")
+
+    # Create a distance matrix and tree from the MSA
     distMatrix = DistanceCalculator("identity").get_distance(bioalign_obj)
+    tree = DistanceTreeConstructor().nj(distMatrix)
+    logging.debug(f"Tree Full:\n{tree}")
 
-    print(distMatrix)
+    # Translate the tree into a list of similar-grouped families
+    groups = generate_family_groups(tree)
 
-    return
+    terminal_dists = terminal_neighbor_dists(tree)
+    print(terminal_dists)
+
+    return groups
